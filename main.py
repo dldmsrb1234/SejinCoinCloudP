1acd4d2323a4b0e203147fe0d93705274d104c4b
dldmsrb1234 <163652106+dldmsrb1234@users.noreply.github.com>
2025-03-20 02:31:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 # --- Firebase 설정 ---
 if not firebase_admin._apps:
-    cred = credentials.Certificate(r"sejincoin-project-firebase-adminsdk-fbsvc-7f7c09536c.json")
+    cred = credentials.Certificate(r"sejincoins2-firebase-adminsdk-fbsvc-2703b60624.json")
 
   # Firebase JSON 키 파일 로드
     firebase_admin.initialize_app(cred)
