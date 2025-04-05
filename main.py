ccbbee4f0575e6b6a27836bf1c6d76dcdcf50b21
dldmsrb1234 <163652106+dldmsrb1234@users.noreply.github.com>
2025-04-05 18:38:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -269,6 +269,7 @@ elif user_type == "학생용":
             unsafe_allow_html=True
         )
         chosen_numbers = st.multiselect("1부터 20까지 숫자 중 **3개**를 선택하세요:", list(range(1, 21)))
+        options = ['1, 2, 3 ,4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20']
         # 선택한 번호 출력: 빨간색 배경, 흰색 텍스트, 글자 크기 150%
         if chosen_numbers:
             chosen_str = ", ".join(map(str, chosen_numbers))
